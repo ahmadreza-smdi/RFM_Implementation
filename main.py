31b8a15292d70cb52d761c294996f0d752ffee88
Ahmadreza Samadi <ahmadreza.smdi@gmail.com>
2020-09-15 19:51:59 +0430

    Update: Recency

--- main.py
+++ main.py
@@ -21,19 +21,31 @@ posts = etree_to_dict(root)
 
 # 2.1 Recency
 
-posts = sorted(posts, key = lambda i: i['CreationDate'])
+posts = sorted(posts, key=lambda i: i['CreationDate'])
 
 number_Of_Segments = 5
 number_Of_Items_In_Segments = int(len(posts)/5)
 
 global list_Counter
-list_Counter=0
+list_Counter = 0
+
 
 for i in range(number_Of_Segments):
     for j in range(number_Of_Items_In_Segments):
         posts[list_Counter]['Recency'] = i+1
-        list_Counter+=1
+        list_Counter += 1
 
+
+sample_Set = []
+for i in range(len(posts)):
+    if 'OwnerUserId' in posts[i]:
+        sample_Set.append(posts[i]['OwnerUserId'])
+
+
+distinct_User = set(sample_Set)
+print(distinct_User)
+# todo find the maximum of recency of each id in posts and append it to target_Segmentation
+target_Segmentation = []
 
 # 2.2 Frequency
 
