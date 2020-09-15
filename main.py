9e118845cd6cdbc2e821c10e27ce41683e788695
Ahmadreza Samadi <ahmadreza.smdi@gmail.com>
2020-09-15 19:06:41 +0430

    Create: Import data from xml + add recency

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-# 1st step: importing data from xml file
-
+import pprint
 import xml.etree.ElementTree as ET
+
+
+# 1st step: importing data from xml file_________________________________________
 
 tree = ET.parse('post.xml')
 root = tree.getroot()
 
-first_Creation_Date = root[0].attrib['CreationDate']
-max_Creation_Date = first_Creation_Date
-for i in range(len(root)):
-    if max_Creation_Date < root[i].attrib['CreationDate']:
-        max_Creation_Date = root[i].attrib['CreationDate']
-    else:
-        pass
-print(max_Creation_Date)
 
-# for i in range(len(root)):
-#     print(root[i].attrib['CreationDate'])
+def etree_to_dict(tree):
+    posts = []
+    for i in range(len(tree)):
+        posts.append(tree[i].attrib)
+    return posts
 
 
-# print(root[1].attrib)
+posts = etree_to_dict(root)
+
+# 2nd step: Adding RFM variables _________________________________________
+
+# 2.1 Recency
+
+posts = sorted(posts, key = lambda i: i['CreationDate'])
+
+number_Of_Segments = 5
+number_Of_Items_In_Segments = int(len(posts)/5)
+
+global list_Counter
+list_Counter=0
+
+for i in range(number_Of_Segments):
+    for j in range(number_Of_Items_In_Segments):
+        posts[list_Counter]['Recency'] = i+1
+        list_Counter+=1
+
+
+# 2.2 Frequency
+
