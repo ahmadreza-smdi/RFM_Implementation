e714422f8b892dbcafaefc93d8a14234489cb1a6
Ahmadreza Samadi <ahmadreza.smdi@gmail.com>
2020-09-16 12:01:20 +0430

    Update: target segmentation updated with Recency

--- main.py
+++ main.py
@@ -28,24 +28,42 @@ number_Of_Items_In_Segments = int(len(posts)/5)
 
 global list_Counter
 list_Counter = 0
+target_Segmentation = []
 
 
 for i in range(number_Of_Segments):
     for j in range(number_Of_Items_In_Segments):
+
         posts[list_Counter]['Recency'] = i+1
+
+        if 'OwnerUserId' in posts[list_Counter]:
+            target_Segmentation.append({'ID': posts[list_Counter]['OwnerUserId'], 'Recency': i+1})
+
         list_Counter += 1
 
 
-sample_Set = []
-for i in range(len(posts)):
-    if 'OwnerUserId' in posts[i]:
-        sample_Set.append(posts[i]['OwnerUserId'])
+#set highest Recency as user's Recency
 
+target_Temp = []
+for x in target_Segmentation:
+    temp = 0
+    for y in target_Segmentation:
+        if x['ID'] == y['ID']:
+            temp = max(x['Recency'], y['Recency'])
+    target_Temp.append({'ID': x['ID'], 'Recency': temp})
 
-distinct_User = set(sample_Set)
-print(distinct_User)
-# todo find the maximum of recency of each id in posts and append it to target_Segmentation
-target_Segmentation = []
+#Remove duplicates in target
+
+seen = set()
+new_l = []
+for d in target_Temp:
+    t = tuple(d.items())
+    if t not in seen:
+        seen.add(t)
+        new_l.append(d)
+
+target_Segmentation = new_l
+
 
 # 2.2 Frequency
 
